def423d0c587031a4b4a77113b76f1e230005f2e
marcel <me@marcelcutts.com>
2016-02-08 23:57:51 +0000

    first commit, FB example with some ES7 upgrades

---
+++ index.ios.js
@@ -0,0 +1,121 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Image
+} from 'react-native';
+
+let API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+let API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+let PAGE_SIZE = 25;
+let PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+let REQUEST_URL = API_URL + PARAMS;
+
+class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+    };
+  }
+
+  async fetchData() {
+    let response = await fetch(REQUEST_URL);
+    let responseData = await response.json();
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+      loaded: true,
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    shadowOffset:{
+      width: 10,
+      height: 10,
+    },
+    shadowColor: 'black',
+    shadowOpacity: 1.0,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
