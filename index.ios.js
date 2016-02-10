429f18895890f5abe10f78738f9cd4f916e9267e
marcel <me@marcelcutts.com>
2016-02-10 23:56:43 +0000

    Started to split out the view into differing components

--- index.ios.js
+++ index.ios.js
@@ -1,116 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View,
-  ListView,
-  Image
-} from 'react-native';
+import React, { AppRegistry, Component } from 'react-native';
+import MovieList from './components/MovieList.js';
 
-
-class MovieMe extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loaded: false,
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-    };
-  }
-
-  async fetchData() {
-    let response = await fetch(REQUEST_URL);
-    let responseData = await response.json();
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-      loaded: true,
-    });
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
+class Container extends Component {
   render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMovie}
-        style={styles.listView}
-      />
-    );
-  }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  }
-
-  renderMovie(movie) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
-      </View>
+      <MovieList />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    shadowOffset:{
-      width: 10,
-      height: 10,
-    },
-    shadowColor: 'black',
-    shadowOpacity: 1.0,
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-});
-
-AppRegistry.registerComponent('MovieMe', () => MovieMe);
+AppRegistry.registerComponent('Container', () => Container);
