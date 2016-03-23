772f82263b2876f545d739e9272dc0049ecd8f68
marcel <me@marcelcutts.com>
2016-03-23 23:57:15 +0000

    Added new actions to deal with initial load via thunk

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,29 @@
+import config from '../config';
+
+const RT_PARAMS = '?apikey=' + config.RT_API_KEY + '&page_limit=' + config.PAGE_SIZE;
+const RT_REQUEST_URL = config.RT_API_URL + RT_PARAMS;
+
 let nextMovieId = 0;
-export const addMovie = (movie) => {
-  return {
-    type: 'ADD_MOVIE',
-    id: nextMovieId++,
-    movie,
+export const addMovie = (movie) => ({
+  type: 'ADD_MOVIE',
+  id: nextMovieId++,
+  movie,
+});
+
+export const requestMovies = () => ({
+  type: 'REQUEST_MOVIES',
+});
+
+export const receiveMovies = (movies) => ({
+  type: 'RECEIVE_MOVIES',
+  receivedAt: Date.now(),
+  movies,
+});
+
+export const fetchMovies = () =>
+  dispatch => {
+    dispatch(requestMovies());
+    return fetch(RT_REQUEST_URL)
+      .then(req => req.json())
+      .then(json => dispatch(receiveMovies(json)));
   };
-};
