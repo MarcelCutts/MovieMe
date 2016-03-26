f6f35376127bf3a7775fba42b876b86b979ef999
marcel <me@marcelcutts.com>
2016-03-26 00:57:51 +0000

    Cool so I turned promises to async/await where possible. Not entirely sure it's an improvement\!

--- actions/index.js
+++ actions/index.js
@@ -30,12 +30,12 @@ export const receiveMoviePoster = (movie, posterUri) => ({
 });
 
 export const fetchMovies = () =>
-  dispatch => {
+  async dispatch => {
     dispatch(requestMovies());
-    return fetch(RT_REQUEST_URL)
-      .then(req => req.json())
-      .then(json => requestMoviePosters(json.movies))
-      .then(movies => dispatch(receiveMovies(movies)));
+    let result = await fetch(RT_REQUEST_URL);
+    let json = await result.json();
+    let movies = await requestMoviePosters(json.movies);
+    dispatch(receiveMovies(movies));
   };
 
 const requestMoviePosters = movies => {
@@ -43,17 +43,19 @@ const requestMoviePosters = movies => {
   return Promise.all(moviesWithPosters);
 };
 
-const requestMoviePoster = movie => {
+const requestMoviePoster = async movie => {
   if (!movie.alternate_ids || !movie.alternate_ids.hasOwnProperty('imdb')) {
     return movie;
   };
 
+  let mdbMovieUrl = generateMdbUrl(movie);
+  let result = await fetch(mdbMovieUrl);
+  let json = await result.json();
+  let mdbPosterUri = MDB_POSTER_URL + json.movie_results[0].poster_path;
+  return { ...movie, mdbPosterUri };
+};
+
+const generateMdbUrl = movie => {
   let imdbId = 'tt' + movie.alternate_ids.imdb;
-  let mdbMovieUrl = MDB_FIND_URL + imdbId + MDB_QUERY_PARAMS + config.MDB_API_KEY;
-  return fetch(mdbMovieUrl)
-    .then(req => req.json())
-    .then(json => {
-      let mdbPosterUri = MDB_POSTER_URL + json.movie_results[0].poster_path;
-      return { ...movie, mdbPosterUri };
-    });
+  return MDB_FIND_URL + imdbId + MDB_QUERY_PARAMS + config.MDB_API_KEY;
 };
